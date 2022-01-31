12df1333196b438e00760c879c60970239ced111
Joy bhattacharya <92808579+Joy-300799@users.noreply.github.com>
2022-01-31 23:45:35 +0530

    project-Quora

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const secretKey = 'project/quora-Joy_Bhattacharya'
 
 
-const userAuth = async(req, res, next) => {
+const userAuth = async (req, res, next) => {
     try {
         const token = req.header('Authorization', 'Bearer Token')
         if (!token) {
@@ -11,16 +11,19 @@ const userAuth = async(req, res, next) => {
         let splitToken = token.split(' ')
 
         let decodeToken = jwt.decode(splitToken[1], secretKey)
-        if (!decodeToken) {
-            return res.status(403).send({ status: false, message: `Invalid authentication token in request ` })
-        }
         if (Date.now() > (decodeToken.exp) * 1000) {
             return res.status(403).send({ status: false, message: `Session Expired, please login again` })
         }
-      
-       req.userId = decodeToken.userId
+
+        let verify = await jwt.verify(splitToken[1], secretKey)
+        if (!verify) {
+            return res.status(403).send({ status: false, message: `Invalid authentication token in request` })
+
+        }
+
+        req.userId = verify.userId
         next()
-       
+
     } catch (err) {
         res.status(500).send({ status: false, message: err.message })
     }
