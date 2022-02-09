831848639458453e0d80dd1f586e39764813ecc3
Joy bhattacharya <joy.arya7381@gmail.com>
2022-02-09 23:55:37 +0530

    Project-Quora

--- src/index.js
+++ src/index.js
@@ -6,13 +6,12 @@ const multer = require('multer')
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(multer().any())
 app.use("/", route);
 
 mongoose
   .connect(
-    "mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/Joy_Bhattacharya-DB?authSource=admin&replicaSet=atlas-wwe75z-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true",
+    "mongodb+srv://Joy-DB:joy123@cluster0.e8rbz.mongodb.net/Quora",
     { useNewUrlParser: true }
   )
   .then(() => console.log("DB connected & Running"))
